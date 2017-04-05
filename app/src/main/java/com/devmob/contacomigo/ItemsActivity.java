3dd3ce35c6daeb77c067a02e6aa3c897c9dd826d
William Lacerda <williamslacerda@gmail.com>
2017-04-05 16:56:58 -0300

    minor

--- app/src/main/java/com/devmob/contacomigo/ItemsActivity.java
+++ app/src/main/java/com/devmob/contacomigo/ItemsActivity.java
@@ -19,8 +19,8 @@ import java.util.LinkedHashMap;
 
 public class ItemsActivity extends AppCompatActivity {
 
-    private LinkedHashMap<String, HeaderInfo> subjects = new LinkedHashMap<String, HeaderInfo>();
-    private ArrayList<HeaderInfo> prodList = new ArrayList<HeaderInfo>();
+    private LinkedHashMap<String, HeaderInfo> productHash = new LinkedHashMap<String, HeaderInfo>();
+    private ArrayList<HeaderInfo> productList = new ArrayList<HeaderInfo>();
 
     private ExpandableListAdapter listAdapter;
     private ExpandableListView itemsExpandableListView;
@@ -29,19 +29,15 @@ public class ItemsActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // add data for displaying in expandable list view
+        //Inicialização
         loadData();
-
-        //get reference of the ExpandableListView
         itemsExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
-        // create the adapter by passing your ArrayList data
-        listAdapter = new ExpandableListAdapter(ItemsActivity.this, prodList);
-        // attach the adapter to the expandable list view
+        listAdapter = new ExpandableListAdapter(ItemsActivity.this, productList);
         itemsExpandableListView.setAdapter(listAdapter);
 
-        //expand all the Groups
-        //expandAll();
+
+
+        //LONG CLICK EM CADA CHILD (PESSOA E PREÇO)
         itemsExpandableListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,7 +45,7 @@ public class ItemsActivity extends AppCompatActivity {
                     int groupPosition = ExpandableListView.getPackedPositionGroup(id);
                     int childPosition = ExpandableListView.getPackedPositionChild(id);
                     //get the group header
-                    HeaderInfo headerInfo = prodList.get(groupPosition);
+                    HeaderInfo headerInfo = productList.get(groupPosition);
                     //get the child info
                     ChildInfo detailInfo = headerInfo.getProductList().get(childPosition);
                     Toast.makeText(ItemsActivity.this, detailInfo.getPersonName() + " deve " + detailInfo.getPrice(), Toast.LENGTH_SHORT).show();
@@ -59,31 +55,22 @@ public class ItemsActivity extends AppCompatActivity {
                 return false;
             }
         });
-        // setOnChildClickListener listener for child row click
+        // CLICK EM CADA CHILD (PESSOA E PREÇO)
         itemsExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                //get the group header
-                HeaderInfo headerInfo = prodList.get(groupPosition);
-                //get the child info
+                HeaderInfo headerInfo = productList.get(groupPosition);
                 ChildInfo detailInfo = headerInfo.getProductList().get(childPosition);
-                //display it or do something with it
-
-                //Toast.makeText(getBaseContext(), " Clicked on :: " + headerInfo.getName()
-                //        + "/" + detailInfo.getPrice(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getBaseContext(), " Clicked on :: " + headerInfo.getName()+ "/" + detailInfo.getPrice(), Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
-        // setOnGroupClickListener listener for group heading click
+        // CLICK EM CADA HEADER (PRODUTO E PREÇO)
         itemsExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                //get the group header
-                HeaderInfo headerInfo = prodList.get(groupPosition);
-                //display it or do something with it
-                //Toast.makeText(getBaseContext(), " Header is :: " + headerInfo.getName(),
-                //        Toast.LENGTH_SHORT).show();
-
+                HeaderInfo headerInfo = productList.get(groupPosition);
+                //Toast.makeText(getBaseContext(), " Header is :: " + headerInfo.getName(),Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
@@ -91,23 +78,7 @@ public class ItemsActivity extends AppCompatActivity {
 
     }
 
-    //method to expand all groups
-    private void expandAll() {
-        int count = listAdapter.getGroupCount();
-        for (int i = 0; i < count; i++) {
-            itemsExpandableListView.expandGroup(i);
-        }
-    }
-
-    //method to collapse all groups
-    private void collapseAll() {
-        int count = listAdapter.getGroupCount();
-        for (int i = 0; i < count; i++) {
-            itemsExpandableListView.collapseGroup(i);
-        }
-    }
-
-    //load some initial data into out list
+    //DATA SETADA POR HARDCODING TEMPORARIO
     private void loadData() {
 
         Person william = new Person("William", 1);
@@ -125,20 +96,9 @@ public class ItemsActivity extends AppCompatActivity {
         addProduct(william, cebola);
         addProduct(daniel, cebola);
 
-
-        //----------------antigo-----------
-        /*
-        addProduct("Batata", "William","11,00");
-        addProduct("Batata", "Silvio","11,00");
-        addProduct("Batata", "Daniel","11,00");
-
-        addProduct("Cebola", "William","14,00");
-        addProduct("Cebola", "Silvio","14,00");
-        */
     }
 
-
-    //here we maintain our products in various departments
+    //PREENCHIMENTO DE CLASSES
     private int addProduct(Person personO, Food food) {
 
         String product = food.getName();
@@ -147,15 +107,16 @@ public class ItemsActivity extends AppCompatActivity {
 
         int groupPosition = 0;
 
-        //check the hash map if the group already exists
-        HeaderInfo headerInfo = subjects.get(product);
-        //add the group if doesn't exists
+        //CHECA SE PRODUTO JA EXISTE
+        HeaderInfo headerInfo = productHash.get(product);
+
+        //CASO NÃO EXISTA, CRIA
         if (headerInfo == null) {
             headerInfo = new HeaderInfo();
             headerInfo.setFood(food);
             headerInfo.setFoodName(product);
-            subjects.put(product, headerInfo);
-            prodList.add(headerInfo);
+            productHash.put(product, headerInfo);
+            productList.add(headerInfo);
         }
 
         //get the children for the group
@@ -174,7 +135,24 @@ public class ItemsActivity extends AppCompatActivity {
         headerInfo.setPeopleList(productList);
 
         //find the group position inside the list
-        groupPosition = prodList.indexOf(headerInfo);
+        groupPosition = this.productList.indexOf(headerInfo);
         return groupPosition;
+    }
+
+
+    //method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            itemsExpandableListView.expandGroup(i);
+        }
+    }
+
+    //method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            itemsExpandableListView.collapseGroup(i);
+        }
     }
 }
