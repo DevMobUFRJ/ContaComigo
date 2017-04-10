e66c026cce2ae76483f167b2459026a256437af7
silviomm <silviommattos@gmail.com>
2017-04-10 11:37:01 -0300

    adding activeandroid

--- app/src/main/java/com/devmob/contacomigo/model/Pessoa.java
+++ app/src/main/java/com/devmob/contacomigo/model/Pessoa.java
@@ -1,17 +1,26 @@
 package com.devmob.contacomigo.model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by silviomm on 05/04/17.
  */
 
-public class Pessoa {
+@Table(name = "Pessoa")
+public class Pessoa extends Model {
 
     private int id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "precoTotal")
     private double precoTotal;
 
     //TODO remove id from constructor, must be auto-incremented(sql)
-    public Pessoa(String nome, int id){
+    public Pessoa(String nome, int id) {
         this.nome = nome;
         this.id = id;
     }
--- app/src/main/java/com/devmob/contacomigo/model/Produto.java
+++ app/src/main/java/com/devmob/contacomigo/model/Produto.java
@@ -1,13 +1,22 @@
 package com.devmob.contacomigo.model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by silviomm on 05/04/17.
  */
 
-public class Produto {
+@Table(name = "produto")
+public class Produto extends Model {
 
     private int id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "preco")
     private double preco;
 
 
