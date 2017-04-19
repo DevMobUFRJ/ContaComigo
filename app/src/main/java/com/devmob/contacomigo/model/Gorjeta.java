1ee4c36abc533aead577389bf42eac7bfb947590
William <williamslacerda@gmail.com>
2017-04-19 12:24:56 -0300

    Alterar valor da gorjeta

--- app/src/main/java/com/devmob/contacomigo/model/Gorjeta.java
+++ app/src/main/java/com/devmob/contacomigo/model/Gorjeta.java
@@ -6,20 +6,30 @@ package com.devmob.contacomigo.model;
 
 public class Gorjeta {
 
+
     private double valor;
+
+    public int getPorcentagem() {
+        return porcentagem;
+    }
+
+    public void setPorcentagem(int porcentagem) {
+        this.porcentagem = porcentagem;
+        this.valor = 1 + (this.porcentagem/100);
+    }
+
+    private int porcentagem;
     private Boolean ativo;
 
     public Gorjeta() {
-        this.valor = 1.1;
+        this.porcentagem = 10;
+        this.valor = 1 + (this.porcentagem/100);
         this.ativo = false;
     }
 
     public double getValor() {
-        return valor;
-    }
 
-    public void setValor(double valor) {
-        this.valor = valor;
+        return valor;
     }
 
     public Boolean getAtivo() {
