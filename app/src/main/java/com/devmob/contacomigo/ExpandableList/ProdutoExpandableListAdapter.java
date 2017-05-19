01bf54d35f749cc432dd1cfc1ba92355094d3b5b
silviomm <silviommattos@gmail.com>
2017-05-19 12:55:36 -0300

    pessoa em produto

--- app/src/main/java/com/devmob/contacomigo/ExpandableList/ProdutoExpandableListAdapter.java
+++ app/src/main/java/com/devmob/contacomigo/ExpandableList/ProdutoExpandableListAdapter.java
@@ -36,9 +36,7 @@ public class ProdutoExpandableListAdapter extends BaseExpandableListAdapter {
         return consumidores.get(indicePessoa);
     }
 
-    public void insereLista(Produto novo) {
-        //prodList.clear();
-        //prodList.addAll(newlist);
+    public void insereProdutoNaLista(Produto novo) {
         prodList.add(novo);
         this.notifyDataSetChanged();
     }
